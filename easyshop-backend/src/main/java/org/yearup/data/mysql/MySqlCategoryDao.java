4e08b029108570fed7a524a653c7abc81f8a7965
Keely Lowder <keelybowman@icloud.com>
2024-12-17 22:12:45 -0500

    added some TODO comments to remember and plan

--- easyshop-backend/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ easyshop-backend/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -12,38 +12,42 @@ import java.util.List;
 @Component
 public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 {
+    private DataSource dataSource;
+
+    // The constructor
     public MySqlCategoryDao(DataSource dataSource)
     {
         super(dataSource);
     }
 
+    // TODO get all categories
     @Override
     public List<Category> getAllCategories()
     {
         // get all categories
         return null;
     }
-
+    // TODO get category by Id
     @Override
     public Category getById(int categoryId)
     {
         // get category by id
         return null;
     }
-
+    // TODO create method
     @Override
     public Category create(Category category)
     {
         // create a new category
         return null;
     }
-
+    // TODO update method
     @Override
     public void update(int categoryId, Category category)
     {
         // update category
     }
-
+    // TODO delete method
     @Override
     public void delete(int categoryId)
     {
